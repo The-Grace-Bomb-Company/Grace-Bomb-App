0bfbdc6f902e87cfc544608ad34a38d9bff6f79d
Drake Lambert <drake-lambert@outlook.com>
2024-02-07 19:57:46 -0600

    generate random text for dropped bombs

--- lib/web_apis/get_dropped_bombs.dart
+++ lib/web_apis/get_dropped_bombs.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:lorem_ipsum_generator/lorem_ipsum_generator.dart';
 
 final random = Random();
 
@@ -12,6 +13,9 @@ Future<List<DroppedBomb>> getDroppedBombs(
   final width = westBoundary - eastBoundary;
 
   generateRandomBomb(int index) => DroppedBomb(
+        random.nextInt(1 << 32).toString(),
+        LoremIpsumGenerator.generate(words: 4),
+        LoremIpsumGenerator.generate(words: 30),
         random.nextDouble() * height + southBoundary,
         random.nextDouble() * width + eastBoundary,
       );
@@ -19,14 +23,29 @@ Future<List<DroppedBomb>> getDroppedBombs(
   await Future.delayed(const Duration(milliseconds: 250));
 
   return List.generate(
-    10,
+    3,
     generateRandomBomb,
   );
 }
 
 class DroppedBomb {
+  final String id;
+  final String title;
+  final String description;
   final double latitude;
   final double longitude;
 
-  DroppedBomb(this.latitude, this.longitude);
+  DroppedBomb(
+      this.id, this.title, this.description, this.latitude, this.longitude);
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is DroppedBomb) {
+      return hashCode == other.hashCode;
+    }
+    return false;
+  }
 }
