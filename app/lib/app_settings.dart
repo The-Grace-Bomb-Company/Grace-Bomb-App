87129f5bd4676e140ebd81ae4a9b112aee7fe594
Ark Fen <arkfen@gmail.com>
2024-08-11 15:46:54 -0400

    Dev Port added for Api in debug mode

--- app/lib/app_settings.dart
+++ app/lib/app_settings.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:global_configuration/global_configuration.dart';
 
 class AppSettings {
-  static final String apiAuthority = getSetting<String>('apiAuthority');
+  static final String apiAuthority = getApiAuthority();
   static final String apiCode = getSetting<String>('apiCode');
   static final getSetting = GlobalConfiguration().getValue;
+
+  static String getApiAuthority() {
+    String uri = getSetting<String>('apiAuthority');
+    if (kDebugMode) {
+      uri += ':${getSetting<String>('apiDevPort')}';
+    }
+    return uri;
+  }
 }
