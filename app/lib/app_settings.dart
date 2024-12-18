4869f13e5aed3d8888f96c2e6f585636dfeab116
Ark Fen <arkfen@gmail.com>
2024-12-18 00:37:31 -0400

    Temp solution for switching between debug and production settings for the App (to use local api)

--- app/lib/app_settings.dart
+++ app/lib/app_settings.dart
@@ -3,6 +3,7 @@ import 'package:global_configuration/global_configuration.dart';
 class AppSettings {
   static final String apiAuthority = getSetting<String>('apiAuthority');
   static final String apiCode = getSetting<String>('apiCode');
+  static final bool isDebug = getSetting<bool>('isDebug');
   static final getSetting = GlobalConfiguration().getValue;
 
   static const int maxBombTitleLength = 30;
